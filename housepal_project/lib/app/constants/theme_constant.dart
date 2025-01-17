8c396c57b5d4d99265c87cb8a0ca57464f94a596
Pramudita <pbhattarai0129@gmail.com>
2025-01-17 21:35:57 +0545

    Hive table created

---
+++ housepal_project/lib/app/constants/theme_constant.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ThemeConstant {
+  ThemeConstant._();
+
+  // Light Theme
+  static const Color lightPrimaryColor = Color(0xFF459D7A); // Primary green color
+  static const Color lightBackgroundColor = Colors.white; // White background
+  static const Color lightAppBarColor = Colors.white; // White app bar
+  static const Color lightTextColor = Colors.black87; // Main text color
+  static const Color lightSecondaryTextColor = Colors.black54; // Secondary text color
+
+  // Dark Theme
+  static const Color darkPrimaryColor = Color(0xFF459D7A); // Primary green color
+  static const Color darkBackgroundColor = Colors.black; // Dark background
+  static const Color darkAppBarColor = Colors.black; // Dark app bar
+  static const Color darkTextColor = Colors.white70; // Main text color
+  static const Color darkSecondaryTextColor = Colors.white60; // Secondary text color
+}
--- housepal_project/lib/main.dart
+++ housepal_project/lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:housepal_project/app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:housepal_project/app/app.dart';
+import 'package:housepal_project/app/di/di.dart';
+import 'package:housepal_project/core/network/hive_service.dart';
+import 'package:housepal_project/features/auth/presentation/view_model/login/login_bloc.dart';
+import 'package:housepal_project/features/auth/presentation/view_model/signup/register_bloc.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HiveService.init();
+  await initDependencies(); // Make sure dependencies are initialized
+ 
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<LoginBloc>(
+          create: (context) => getIt<LoginBloc>(), // Providing LoginBloc
+        ),
+        BlocProvider<RegisterBloc>(
+          create: (context) =>
+              getIt<RegisterBloc>(), // Provide RegisterBloc here
+        ),
+        // Other providers can go here
+      ],
+      child: const App(),
+    ),
+  );
 }
